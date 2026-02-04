88f20d0f7977e780c880b76c3d1aea70f3d97600
Howie <e1384345@u.nus.edu>
2026-02-04 21:47:23 +0800

    Completed A-CodingStandard

--- src/main/java/Idecapitator.java
+++ src/main/java/Idecapitator.java
@@ -6,40 +6,49 @@ public class Idecapitator {
         Task[] tasks = new Task[100];
         int taskCount = 0;
 
-        System.out.println(line + "\nHello! I'm Idecapitator\nWhat can I do for you?\n" + line);
+        System.out.println(line);
+        System.out.println("Hello! I'm Idecapitator");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
 
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             String input = scanner.nextLine();
+            String command = input.split(" ")[0];
             System.out.println(line);
 
-            if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!\n" + line);
-                break;
-            } else if (input.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + "." + tasks[i].toString());
-                }
-            } else if (input.startsWith("mark")) {
-                int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
-                tasks[taskIndex].markAsDone();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  " + tasks[taskIndex]);
-            } else if (input.startsWith("unmark")) {
-                int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
-                tasks[taskIndex].unmarkAsDone();
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("  " + tasks[taskIndex]);
-            } else {
-                tasks[taskCount] = new Task(input);
-                taskCount++;
-                System.out.println("added: " + input);
+            switch (command) {
+                case "bye":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println(line);
+                    scanner.close();
+                    return;
+                case "list":
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + "." + tasks[i].toString());
+                    }
+                    break;
+                case "mark":
+                    int markIdx = Integer.parseInt(input.split(" ")[1]) - 1;
+                    tasks[markIdx].markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  " + tasks[markIdx]);
+                    break;
+                case "unmark":
+                    int unmarkIdx = Integer.parseInt(input.split(" ")[1]) - 1;
+                    tasks[unmarkIdx].unmarkAsDone();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("  " + tasks[unmarkIdx]);
+                    break;
+                default:
+                    tasks[taskCount] = new Task(input);
+                    taskCount++;
+                    System.out.println("added: " + input);
+                    break;
             }
             System.out.println(line);
         }
-        scanner.close();
     }
 }
-
