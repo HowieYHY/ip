3bf816829acf7976dbb915e0a3937e8078b9bd83
Howie <e1384345@u.nus.edu>
2026-02-04 21:42:15 +0800

    Completed level 3

--- src/main/java/Idecapitator.java
+++ src/main/java/Idecapitator.java
@@ -3,13 +3,10 @@ import java.util.Scanner;
 public class Idecapitator {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
-        System.out.println(line);
-        System.out.println("Hello! I'm Idecapitator");
-        System.out.println("What can I do for you?");
-        System.out.println(line);
+        System.out.println(line + "\nHello! I'm Idecapitator\nWhat can I do for you?\n" + line);
 
         Scanner scanner = new Scanner(System.in);
 
@@ -18,22 +15,31 @@ public class Idecapitator {
             System.out.println(line);
 
             if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!\n" + line);
                 break;
             } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + "." + tasks[i].toString());
                 }
-                System.out.println(line);
+            } else if (input.startsWith("mark")) {
+                int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks[taskIndex].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + tasks[taskIndex]);
+            } else if (input.startsWith("unmark")) {
+                int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks[taskIndex].unmarkAsDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + tasks[taskIndex]);
             } else {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("added: " + input);
-                System.out.println(line);
             }
+            System.out.println(line);
         }
-
         scanner.close();
     }
 }
+
