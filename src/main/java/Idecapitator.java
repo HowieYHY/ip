a7d31f218effaeaefc186cc2601d2b46eabc54af
Howie <e1384345@u.nus.edu>
2026-02-04 21:29:42 +0800

    Completed level 2

--- src/main/java/Idecapitator.java
+++ src/main/java/Idecapitator.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Idecapitator {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         System.out.println(line);
         System.out.println("Hello! I'm Idecapitator");
@@ -13,17 +15,23 @@ public class Idecapitator {
 
         while (true) {
             String input = scanner.nextLine();
-
             System.out.println(line);
 
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(line);
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(line);
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("added: " + input);
+                System.out.println(line);
             }
-
-            System.out.println(input);
-            System.out.println(line);
         }
 
         scanner.close();
