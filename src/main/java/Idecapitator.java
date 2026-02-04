c9625edf5b4fc7d218c1630c662d61aee8b8cc45
Howie <e1384345@u.nus.edu>
2026-02-04 21:58:53 +0800

    Completed Level 4

--- src/main/java/Idecapitator.java
+++ src/main/java/Idecapitator.java
@@ -2,14 +2,11 @@ import java.util.Scanner;
 
 public class Idecapitator {
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
+        String line = "    ____________________________________________________________";
         Task[] tasks = new Task[100];
         int taskCount = 0;
 
-        System.out.println(line);
-        System.out.println("Hello! I'm Idecapitator");
-        System.out.println("What can I do for you?");
-        System.out.println(line);
+        System.out.println(line + "\n    Hello! I'm Idecapitator\n    What can I do for you?\n" + line);
 
         Scanner scanner = new Scanner(System.in);
 
@@ -20,35 +17,54 @@ public class Idecapitator {
 
             switch (command) {
                 case "bye":
-                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println("    Bye. Hope to see you again soon!");
                     System.out.println(line);
                     scanner.close();
                     return;
                 case "list":
-                    System.out.println("Here are the tasks in your list:");
+                    System.out.println("    Here are the tasks in your list:");
                     for (int i = 0; i < taskCount; i++) {
-                        System.out.println((i + 1) + "." + tasks[i].toString());
+                        System.out.println("    " + (i + 1) + "." + tasks[i]);
                     }
                     break;
                 case "mark":
-                    int markIdx = Integer.parseInt(input.split(" ")[1]) - 1;
-                    tasks[markIdx].markAsDone();
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("  " + tasks[markIdx]);
+                    int mIdx = Integer.parseInt(input.split(" ")[1]) - 1;
+                    tasks[mIdx].markAsDone();
+                    System.out.println("    Nice! I've marked this task as done:\n      " + tasks[mIdx]);
                     break;
                 case "unmark":
-                    int unmarkIdx = Integer.parseInt(input.split(" ")[1]) - 1;
-                    tasks[unmarkIdx].unmarkAsDone();
-                    System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println("  " + tasks[unmarkIdx]);
+                    int uIdx = Integer.parseInt(input.split(" ")[1]) - 1;
+                    tasks[uIdx].unmarkAsDone();
+                    System.out.println("    OK, I've marked this task as not done yet:\n      " + tasks[uIdx]);
+                    break;
+                case "todo":
+                    tasks[taskCount] = new Todo(input.substring(5));
+                    taskCount++;
+                    printAddedMessage(tasks[taskCount - 1], taskCount);
+                    break;
+                case "deadline":
+                    String[] dParts = input.substring(9).split(" /by ");
+                    tasks[taskCount] = new Deadline(dParts[0], dParts[1]);
+                    taskCount++;
+                    printAddedMessage(tasks[taskCount - 1], taskCount);
+                    break;
+                case "event":
+                    String[] eParts = input.substring(6).split(" /from | /to ");
+                    tasks[taskCount] = new Event(eParts[0], eParts[1], eParts[2]);
+                    taskCount++;
+                    printAddedMessage(tasks[taskCount - 1], taskCount);
                     break;
                 default:
-                    tasks[taskCount] = new Task(input);
-                    taskCount++;
-                    System.out.println("added: " + input);
+                    System.out.println("    I'm sorry, I don't know what that means.");
                     break;
             }
             System.out.println(line);
         }
+    }
+
+    private static void printAddedMessage(Task task, int count) {
+        System.out.println("    Got it. I've added this task:");
+        System.out.println("      " + task);
+        System.out.println("    Now you have " + count + " tasks in the list.");
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,5 @@
-public class Task {
+// Base Task Class
+class Task {
     protected String description;
     protected boolean isDone;
 
@@ -22,5 +23,46 @@ public class Task {
     @Override
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
+    }
+}
+
+class Todo extends Task {
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+class Deadline extends Task {
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
+
+class Event extends Task {
+    protected String from;
+    protected String to;
+
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
 }
